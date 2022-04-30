7ebc8bf75459c3cb65ce6b724e7814f98277ed10
e.guarino@goreply.io <eugenio-97@live.it>
2022-04-30 21:49:35 +0200

    feat(mc): startup script

--- terraform/startup-script.sh
+++ terraform/startup-script.sh
@@ -2,3 +2,11 @@
 bash
 sudo su
 echo "hello"
+
+wget https://launcher.mojang.com/v1/objects/3737db93722a9e39eeada7c27e7aca28b144ffa7/server.jar -O minecraft_server.1.17.jar
+
+java -Xms1024M -Xmx1536M -jar minecraft_server.1.18.jar -o true
+
+chmod +x /home/minecraft/run.sh
+
+# sudo sh /home/minecraft/run.sh
