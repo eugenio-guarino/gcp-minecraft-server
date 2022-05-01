e3b47efe5f5aec81143bd1006334f7df5285a4d7
e.guarino@goreply.io <eugenio-97@live.it>
2022-05-01 02:13:48 +0200

    ci(pipeline): add bootstrap script

--- terraform/startup-script.sh
+++ terraform/startup-script.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
-bash
 sudo su
-echo "hello"
 
-wget https://launcher.mojang.com/v1/objects/3737db93722a9e39eeada7c27e7aca28b144ffa7/server.jar -O minecraft_server.1.17.jar
+## install packages
+apt update && sudo apt upgrade
+apt install default-jre
+apt install unzip
+sudo apt install openjdk-17-jdk
 
-java -Xms1024M -Xmx1536M -jar minecraft_server.1.18.jar -o true
+# download minecraft server
+wget https://maven.minecraftforge.net/net/minecraftforge/forge/1.18.2-40.1.0/forge-1.18.2-40.1.0-installer.jar
 
-chmod +x /home/minecraft/run.sh
+# copy mods 
+mkdir /opt/minecraft
+mv forge-1.18.2-40.1.0-installer.jar /opt/minecraft
 
-# sudo sh /home/minecraft/run.sh
+cd /opt/minecraft
+
+# run java
+java -Xms1024M -Xmx7000M -jar forge-1.18.2-40.1.0-installer.jar --installServer
+
+echo eula=true>eula.txt
+
+#run minecraft server
+chmod +x minecraft/run.sh
+sh minecraft/run.sh &
+
+# download mods
+gsutil -m cp -r gs://minecraft-server-storage-bucket/mods.zip /opt/minecraft/
+unzip mods.zip
