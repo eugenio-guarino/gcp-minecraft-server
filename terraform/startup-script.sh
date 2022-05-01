0eee067d93f12914e5a1ae4faef2acc6d19b63ea
e.guarino@goreply.io <eugenio-97@live.it>
2022-05-01 03:28:06 +0200

    ci(bootstrap-script): download server.properties

--- terraform/startup-script.sh
+++ terraform/startup-script.sh
@@ -11,7 +11,7 @@ apt install openjdk-17-jdk -y
 wget https://maven.minecraftforge.net/net/minecraftforge/forge/1.18.2-40.1.0/forge-1.18.2-40.1.0-installer.jar
 
 # create directory
-mkdir /opt/minecraft
+mkdir -p /opt/minecraft
 
 # move forge in new directory
 mv forge-1.18.2-40.1.0-installer.jar /opt/minecraft
@@ -23,10 +23,20 @@ java -Xms1024M -Xmx7000M -jar forge-1.18.2-40.1.0-installer.jar --installServer
 
 echo eula=true>eula.txt
 
-#run minecraft server
-chmod +x run.sh
-sh run.sh &
-
 # download mods
 gsutil -m cp -r gs://minecraft-server-storage-bucket/mods.zip /opt/minecraft/
 unzip mods.zip
+
+mkdir -p world/datapacks
+
+# download datapack
+gsutil -m cp -r gs://minecraft-server-storage-bucket/mods.zip /opt/minecraft/world/datapacks
+unzip mods.zip
+rm mods.zip
+
+# download server.properties
+gsutil -m cp -r gs://minecraft-server-storage-bucket/mods.zip /opt/minecraft/
+
+#run minecraft server
+chmod +x run.sh
+sh run.sh &
