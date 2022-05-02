b7b4f53c1a9ca4d9ec98231ad38558d8e5b5fe42
e.guarino@goreply.io <eugenio-97@live.it>
2022-05-02 16:05:04 +0200

    ci(instance-script): change machine type

--- terraform/startup-script.sh
+++ terraform/startup-script.sh
@@ -19,7 +19,7 @@ mv forge-1.18.2-40.1.0-installer.jar /opt/minecraft
 
 cd /opt/minecraft
 
-# run java
+# run java with appropriate flags
 java -XX:+UseG1GC -Xmx15G -Xms15G -XX:InitiatingHeapOccupancyPercent=20 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:G1NewSizePercent=40 -XX:G1MaxNewSizePercent=50 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32 -jar forge-1.18.2-40.1.0-installer.jar --installServer
 
 echo eula=true>eula.txt
@@ -30,18 +30,21 @@ unzip mods.zip
 rm mods.zip
 
 # download datapack
-gsutil -m cp -r gs://minecraft-server-storage-bucket/Aria.zip /opt/minecraft/
+gsutil -m cp gs://minecraft-server-storage-bucket/Aria.zip /opt/minecraft/
 unzip /opt/minecraft/Aria.zip
 rm Aria.zip
 
 # download server.properties
-gsutil -m cp -r gs://minecraft-server-storage-bucket/server.properties /opt/minecraft/
+gsutil -m cp gs://minecraft-server-storage-bucket/server.properties /opt/minecraft/
 
 #user cache
-gsutil -m cp -r gs://minecraft-server-storage-bucket/usercache.json /opt/minecraft/
-gsutil -m cp -r gs://minecraft-server-storage-bucket/auto-destroy.sh /opt/minecraft/
+gsutil -m cp gs://minecraft-server-storage-bucket/usercache.json /opt/minecraft/
 
-#run minecraft server
+# auto-destroy script
+gsutil -m cp gs://minecraft-server-storage-bucket/auto-destroy.sh /opt/minecraft/
+
+# run minecraft server
 chmod +x run.sh
 chmod +x auto-destroy.sh
 # sh run.sh &
+# sh auto-destroy.sh &
