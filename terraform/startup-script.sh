d2d22ad710463118e4817f2bdcaf4f0129cf98f5
e.guarino@goreply.io <eugenio-97@live.it>
2022-05-02 16:46:00 +0200

    feat(processes): run scripts with nohup

--- terraform/startup-script.sh
+++ terraform/startup-script.sh
@@ -8,36 +8,32 @@ apt install unzip -y
 apt install openjdk-17-jdk -y
 apt install zip -y
 
+# create directory
+mkdir -p /opt/minecraft
+cd /opt/minecraft
+
 # download minecraft server
 wget https://maven.minecraftforge.net/net/minecraftforge/forge/1.18.2-40.1.0/forge-1.18.2-40.1.0-installer.jar
-
-# create directory
-mkdir -p /opt/minecraft/Aria/datapacks
-
-# move forge in new directory
-mv forge-1.18.2-40.1.0-installer.jar /opt/minecraft
-
-cd /opt/minecraft
 
 # run java with appropriate flags
 java -XX:+UseG1GC -Xmx15G -Xms15G -XX:InitiatingHeapOccupancyPercent=20 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:G1NewSizePercent=40 -XX:G1MaxNewSizePercent=50 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32 -jar forge-1.18.2-40.1.0-installer.jar --installServer
 
 echo eula=true>eula.txt
 
-# download mods
+# download mods from cloud storage
 gsutil -m cp -r gs://minecraft-server-storage-bucket/mods.zip /opt/minecraft/
 unzip mods.zip
 rm mods.zip
 
-# download datapack
+# download world from cloud storage
 gsutil -m cp gs://minecraft-server-storage-bucket/Aria.zip /opt/minecraft/
 unzip /opt/minecraft/Aria.zip
 rm Aria.zip
 
-# download server.properties
+# download server.properties from cloud storage
 gsutil -m cp gs://minecraft-server-storage-bucket/server.properties /opt/minecraft/
 
-#user cache
+# download user cache from cloud storage
 gsutil -m cp gs://minecraft-server-storage-bucket/usercache.json /opt/minecraft/
 
 # auto-destroy script
@@ -46,5 +42,7 @@ gsutil -m cp gs://minecraft-server-storage-bucket/auto-destroy.sh /opt/minecraft
 # run minecraft server
 chmod +x run.sh
 chmod +x auto-destroy.sh
-# sh run.sh &
-# sh auto-destroy.sh &
+
+nohup 'bash run.sh' &
+
+nohup watch -n5 'auto-destroy.sh' &
