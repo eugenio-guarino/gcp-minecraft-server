3d4cf4ef6ae6a62e58a1888d4fefe0a244b65945
e.guarino@goreply.io <eugenio-97@live.it>
2022-05-02 15:59:30 +0200

    ci(instance-script): update java script

--- terraform/startup-script.sh
+++ terraform/startup-script.sh
@@ -20,7 +20,7 @@ mv forge-1.18.2-40.1.0-installer.jar /opt/minecraft
 cd /opt/minecraft
 
 # run java
-java -Xms1G -Xmx16G -jar forge-1.18.2-40.1.0-installer.jar --installServer
+java -XX:+UseG1GC -Xmx15G -Xms15G -XX:InitiatingHeapOccupancyPercent=20 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:G1NewSizePercent=40 -XX:G1MaxNewSizePercent=50 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32 -jar forge-1.18.2-40.1.0-installer.jar --installServer
 
 echo eula=true>eula.txt
 
@@ -39,9 +39,9 @@ gsutil -m cp -r gs://minecraft-server-storage-bucket/server.properties /opt/mine
 
 #user cache
 gsutil -m cp -r gs://minecraft-server-storage-bucket/usercache.json /opt/minecraft/
-gsutil -m cp -r gs://minecraft-server-storage-bucket/auto-shutdown.sh /opt/minecraft/
+gsutil -m cp -r gs://minecraft-server-storage-bucket/auto-destroy.sh /opt/minecraft/
 
 #run minecraft server
 chmod +x run.sh
-chmod +x auto-shutdown.sh
+chmod +x auto-destroy.sh
 # sh run.sh &
