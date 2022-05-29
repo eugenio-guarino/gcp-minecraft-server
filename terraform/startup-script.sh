cdd56441a866f073eb33d02f52cd18abcb84e8c4
e.guarino@goreply.io <eugenio-97@live.it>
2022-05-29 16:17:59 +0200

    feat(startup-script): add delays to avoid server shutting off too quickly

--- terraform/startup-script.sh
+++ terraform/startup-script.sh
@@ -54,5 +54,9 @@ gsutil -m cp gs://minecraft-server-storage-bucket/notify.sh .
 chmod +x run.sh
 
 nohup bash run.sh </dev/null &>/dev/null &
-nohup bash auto-destroy.sh </dev/null &>/dev/null &
+
+sleep 5m
 nohup bash notify.sh </dev/null &>/dev/null &
+
+sleep 10m
+nohup bash auto-destroy.sh </dev/null &>/dev/null &
