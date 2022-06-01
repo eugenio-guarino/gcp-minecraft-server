f458dd5d969711179951e8f886e6ac6acb538690
e.guarino@goreply.io <eugenio-97@live.it>
2022-06-01 20:25:41 +0200

    feat(startup-script): update garbage collector flags

--- terraform/startup-script.sh
+++ terraform/startup-script.sh
@@ -17,7 +17,7 @@ cd /opt/minecraft
 wget https://maven.minecraftforge.net/net/minecraftforge/forge/1.18.2-40.1.0/forge-1.18.2-40.1.0-installer.jar
 
 # run java with appropriate flags
-java -XX:+UseG1GC -Xmx15G -Xms15G -XX:InitiatingHeapOccupancyPercent=20 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:G1NewSizePercent=40 -XX:G1MaxNewSizePercent=50 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32 -jar forge-1.18.2-40.1.0-installer.jar --installServer
+java -XX:+UseG1GC -Xmx6500M -Xms6500M -XX:InitiatingHeapOccupancyPercent=20 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:G1NewSizePercent=40 -XX:G1MaxNewSizePercent=50 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32 -jar forge-1.18.2-40.1.0-installer.jar --installServer
 
 echo eula=true>eula.txt
 
